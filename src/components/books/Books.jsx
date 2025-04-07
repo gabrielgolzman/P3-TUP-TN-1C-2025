6ccf53b6a96ff4c80e65be5783f6236f266efa3b
Gabriel Golzman <ggolzman@primerosystems.com>
2025-04-07 19:50:24 -0300

    Added search books

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
+
 import BookItem from "../bookItem/BookItem";
+import BookSearch from "../bookSearch/BookSearch";
+
 
 const Books = ({ books }) => {
     const [selectedBook, setSelectedBook] = useState('');
+    const [searchBook, setSearchBook] = useState("")
+
+    const handleBookSearch = (value) => {
+        setSearchBook(value)
+    }
     const handleBookSelected = (bookTitle) => {
         setSelectedBook(bookTitle);
     }
-    const booksMapped = books.map((book) =>
-        <BookItem
-            key={book.id}
-            title={book.title}
-            author={book.author}
-            pageCount={book.pageCount}
-            rating={book.rating}
-            imageUrl={book.imageUrl}
-            available={book.available}
-            onBookSelected={handleBookSelected} />);
+    const booksMapped = books
+        .filter(book =>
+            book.title?.toUpperCase().includes(searchBook.toUpperCase()))
+        .map((book) =>
+            <BookItem
+                key={book.id}
+                title={book.title}
+                author={book.author}
+                pageCount={book.pageCount}
+                rating={book.rating}
+                imageUrl={book.imageUrl}
+                available={book.available}
+                onBookSelected={handleBookSelected} />);
 
     return (
-        <>{
-            selectedBook &&
-            <p>Usted ha seleccionado el libro: <b>{selectedBook}</b></p>
-        }
+        <>
+
+            <BookSearch onSearch={handleBookSearch} searchBook={searchBook} />
+            {
+                selectedBook &&
+                <p>Usted ha seleccionado el libro: <b>{selectedBook}</b></p>
+            }
 
             <div className="d-flex justify-content-center flex-wrap">
-                {booksMapped}
+                {booksMapped.length ?
+                    booksMapped :
+                    <p>No se encontraron libros</p>}
             </div>
         </>)
 }
